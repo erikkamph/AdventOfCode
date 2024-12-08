ea09252ac3b01a339e5b0d5c58f07a4807af10d6
Erik <48324322+erikkamph@users.noreply.github.com>
2024-12-08 19:10:53 +0100

    Removed nargs

--- 2024/modules/parser.py
+++ 2024/modules/parser.py
@@ -36,7 +36,6 @@ parser.add_argument("-o", "--output",
 parser.add_argument("-p", "--part",
                     dest='part',
                     help='Run part 1 or 2',
-                    nargs=1,
                     type=int)
 
 def parse():
